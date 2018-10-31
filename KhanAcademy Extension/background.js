c99502b3f48154ba3725e94ed022aed975053773
iamaureen <ishrat.ahmed@asu.edu>
2018-10-31 00:22:53 -0400

    fixed error & updated KA to log user input

--- KhanAcademy Extension/background.js
+++ KhanAcademy Extension/background.js
@@ -1,30 +1,38 @@
 
 // background.js
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
-});
-
-// This block is new!
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
-
-
-//fire when an internal link is clicked. only url is changed, page not completely reloaded
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  //https://developer.chrome.com/extensions/webNavigation
-  console.log('reason for page navigation:', details.transitionType) //check background console
-  //execute content script at page url change else content script is injected only the first time
-  chrome.tabs.executeScript(null, {file:"content.js"});
-  
-});
+// 
+// // Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // Send a message to the active tab
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     var activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//   });
+// });
+//
+// // This block is new!
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "open_new_tab" ) {
+//       chrome.tabs.create({"url": request.url});
+//     }
+//   }
+// );
+//
+//
+// //fire when an internal link is clicked. only url is changed, page not completely reloaded
+// chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+//   //https://developer.chrome.com/extensions/webNavigation
+//   console.log('reason for page navigation:', details.transitionType) //check background console
+//   //execute content script at page url change else content script is injected only the first time
+//   chrome.tabs.executeScript(null, {file:"content.js"});
+//
+// });
+//
+// function handleMessage(request, sender, sendResponse) {
+//   console.log("Message from the content script: " +
+//     request.greeting);
+//   //sendResponse({response: "Response from background script"});
+// }
+//
+// chrome.runtime.onMessage.addListener(handleMessage);
--- KhanAcademy Extension/content.js
+++ KhanAcademy Extension/content.js
@@ -17,7 +17,8 @@
 console.clear();
 //code starts from here: IA
 //everytime the page loads, it will print the following, only once.
-console.log("page url ::", document.location.href)
+console.log("page url :: ", document.location.href)
+
 
 
 //$(window).bind("load", function() { //works the first time page loads. since it is already loaded next time,
@@ -25,13 +26,17 @@ console.log("page url ::", document.location.href)
 
   // code here
   setTimeout(function(){
+
     console.log("line 28 :: im done loading!");
+
+    var username = $(".nicknameDisplay_1i9m8bl").html();
+    console.log("name :: ", username)
     //console.log('line 29 :: ',$('a.show-answer-form'));
     //show the entire discussion thread on this page
     console.log('line 30 :: discussion thread',$("div.default_olfzxm-o_O-container_k9kem1"));
     //total number of visible questions in this page - without pressing the show more button
     console.log('line 33 :: total questions posted ', $(".post_b12blo").length)
-    console.log($('line 33 :: ul.listWrapper_17u7rry li').length)
+    console.log('line 33 :: ', $("ul.listWrapper_17u7rry li").length)
     //div class for both question post and answer post
     console.log('line 35 :: ', $("div.default_olfzxm-o_O-everythingButTheAvatar_py3uvl").length);
     //div class for button classes
@@ -42,6 +47,11 @@ console.log("page url ::", document.location.href)
 
     //detect click 'answer this question'
     $("div.default_olfzxm-o_O-postFooter_16ru1fn button.linkStyledButton_ycurob").off().click(function(e){
+      var action = "";
+      var type = "";
+      var input = "";
+      var pagenumber = "";
+      data = {};
 
       //clicked item
       var clicked = $(e.target);
@@ -51,89 +61,98 @@ console.log("page url ::", document.location.href)
       var thread = clicked.closest("div.default_olfzxm-o_O-everythingButTheAvatar_py3uvl");
       console.log(thread);
       //console.log('action: click, action_type: answer question')
+      data['username'] = thread[0].children[0].children[0].children[0].children[0].children[0].innerText;
       console.log('username who posted: ',thread[0].children[0].children[0].children[0].children[0].children[0].innerText);
       //console.log(thread[0].children[2].childElementCount)
       //check if question comment is clicked or answer comment - both has the same classname, so identifying using number of children
       if(thread[0].children[2].childElementCount>3){
         console.log('type :: question reply/comment')
+        action = 'question reply/comment'
+        data['votes'] = thread[0].children[2].children[4].children[0].innerHTM;
         console.log('number of votes for this post: ',thread[0].children[2].children[4].children[0].innerHTML);
       }else{
+        action = 'answer comment'
+        data['votes'] = thread[0].children[2].children[0].children[0].innerText;
         console.log('type :: question reply/answer comment')
         console.log('number of votes for this post: ',thread[0].children[2].children[0].children[0].innerText);
       }
 
+      data['content'] = thread[0].children[1].children[0].children[0].innerText;
       console.log('question content: ',thread[0].children[1].children[0].children[0].innerText);
+
+      console.log(JSON.stringify({
+              'action': action,
+              'type': type,
+              'input': data,
+              'pagenumber': 121212//in case of extension pagenumber is current link
+            }))
+
+      $.ajax({
+
+         type: 'POST',
+         url: 'http://127.0.0.1:8000/extensionlog/', //this works
+         //url: 'https://hcilabasu.pythonanywhere.com/extensionlog/',
+         data: JSON.stringify({
+                 'username':username,
+                 'action': action,
+                 'type': type,
+                 'input': data,
+                 'pagenumber': 121212//in case of extension pagenumber is current link
+               }),
+         success: function (sdata, textStatus, xhr) {
+                   console.log(sdata);
+           },
+           error: function (xhr, textStatus, errorThrown) {
+                     console.log(xhr);
+                     alert(xhr);
+       }
+
+      }); //end of ajax call
 
     });
     //detect student attempt to write answer
     // $('textarea').click(function(e){
     //   console.log($('textarea').length);
     // })
-    $('textarea').on('change', function(e){
+    $('textarea').off().on('change', function(e){
       console.log('user attemped to answer :: ', $(this).val()); //TODO: save input
+      data['input'] = $(this).val()
+      $.ajax({
+
+         type: 'POST',
+         //url: 'http://127.0.0.1:8000/extensionlog/', //this works
+         url: 'https://hcilabasu.pythonanywhere.com/extensionlog/',
+         data: JSON.stringify({
+                 'username':username,
+                 'action': 'user attemped to answer',
+                 'type': 'user input',
+                 'input': data,
+                 'pagenumber': 121212//in case of extension pagenumber is current link
+               }),
+         success: function (sdata, textStatus, xhr) {
+                   console.log(sdata);
+           },
+           error: function (xhr, textStatus, errorThrown) {
+                     console.log(xhr);
+                     alert(xhr);
+       }
+
+      }); //end of ajax call
 
 
     })
     //detect user click on 'more' that loads additional questions in the page
-    $('button.button_1eqj1ga-o_O-shared_1gi2itp-o_O-default_9fm203').on('click',function(e){
+    $('button.button_1eqj1ga-o_O-shared_1gi2itp-o_O-default_9fm203-o_O-button_1ktyqnj').on('click',function(e){
       console.log($('ul.listWrapper_17u7rry li').length)
 
     });
+    // button.button_1eqj1ga-o_O-shared_1gi2itp-o_O-disabled_e07bai-o_O-default_9fm203-o_O-disabled_h5jnhu-o_O-button_1ktyqnj
+    //
+    // button.button_1eqj1ga-o_O-shared_1gi2itp-o_O-default_9fm203-o_O-button_1ktyqnj
 
-    //capture 'answer' button click with 'answer' content
-    var answer_btn = $('input.simple-button.primary.discussion-submit');
-    answer_btn.on('click', function(e){
-      var clicked = $(e.target).closest('.thread') //returns div discussion-meta
-      console.log(clicked)
-      console.log('action: click, action_type: submit answer')
-      var input = $('textarea.discussion-text.open')
-      //console.log('question : ', clicked[0].innerText)
-      console.log('answer : ',input.val().trim());
-    });
-
-  //   $.ajax({
-  //     type:'GET',
-  //     url:'http://127.0.0.1:8000/extensionlog/',
-  //     error: function (xhr, textStatus, errorThrown) {
-  //               console.log(xhr);
-  // }
-  //   });
-
-    $('a.toggle-replies.show-replies').click(function(e){
-      //var clicked = $(e.target.parentElement); //returns div discussion-meta-controls
-      //var clicked = $(e.target).closest('.answers') //returns div answers
-      var clicked = $(e.target).closest('.discussion-meta').siblings('.discussion-content') //returns div discussion-content
-      console.log(clicked)
-
-      console.log('action: click, action_type: check comment')
-      console.log('content:', clicked[0].innerText);
-
-      //track user log using ajax request
-      //TODO: can you call 'enterLogIntoDatabase' method of server??? check!
+    console.log('line 83 :: ',$("button.button_1eqj1ga-o_O-shared_1gi2itp-o_O-default_9fm203-o_O-button_1ktyqnj").length)
 
 
-      $.ajax({
-       type: 'POST',
-
-       url: 'http://127.0.0.1:8000/extensionlog/',
-       // headers: {'X-CSRFToken': '{{ csrf_token }}'},
-       data: JSON.stringify({
-               'action': 'click',
-               'type': 'check comment',
-               'input': clicked[0].innerText,
-               'pagenumber': document.location.href//in case of extension pagenumber is current link
-             }),
-       success: function (data, textStatus, xhr) {
-                 console.log(data);
-         },
-         error: function (xhr, textStatus, errorThrown) {
-                   console.log(xhr);
-     }
-
- }); //end of ajax call
-
-
-    });
 
   }, 5000);
 
